8044d1643f427d63a8e3c73c145399d2afdc2b24
GUSTAV0-CRUZ <gustavocruzsantos39@gmail.com>
2026-03-30 14:12:19 -0300

    feat(): adiciona dependencias iniciais e configura o main dos microservice com as filas que serão usadas

--- service-payment/src/main.ts
+++ service-payment/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.RMQ,
+      options: {
+        noAck: false,
+        queue: 'service-payment',
+        urls: [String(process.env.REBBITMQ_URL)],
+        queueOptions: {
+          durable: true,
+        },
+      },
+    },
+  );
+  await app.listen();
 }
 bootstrap();

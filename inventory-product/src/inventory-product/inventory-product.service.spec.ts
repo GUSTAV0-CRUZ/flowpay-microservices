b7662003db917e4688bf2a288517fa980d04109f
GUSTAV0-CRUZ <gustavocruzsantos39@gmail.com>
2026-03-31 22:58:39 -0300

    test(inventory-product): testa o metodo findOne do InventoryProductService

--- inventory-product/src/inventory-product/inventory-product.service.spec.ts
+++ inventory-product/src/inventory-product/inventory-product.service.spec.ts
@@ -23,6 +23,7 @@ describe('InventoryProductService', () => {
           provide: ProductRepository,
           useValue: {
             findAll: jest.fn(),
+            findOneById: jest.fn(),
           },
         },
       ],
@@ -56,6 +57,58 @@ describe('InventoryProductService', () => {
       jest.spyOn(productRepository, 'findAll').mockRejectedValue(new Error());
 
       await expect(inventoryProductService.findAll()).rejects.toThrow(
+        RpcException,
+      );
+    });
+  });
+
+  describe('findOne', () => {
+    it('shold return one product', async () => {
+      const product = createProduct();
+      const id = 'id123';
+
+      jest
+        .spyOn(productRepository, 'findOneById')
+        .mockResolvedValue(product as any);
+
+      const result = await inventoryProductService.findOne(id);
+
+      expect(productRepository.findOneById).toHaveBeenCalledTimes(1);
+      expect(productRepository.findOneById).toHaveBeenCalledWith(id);
+      expect(result).toEqual(product);
+    });
+
+    it('shold return error "product not found"', async () => {
+      jest
+        .spyOn(productRepository, 'findOneById')
+        .mockResolvedValue(undefined as any);
+
+      await expect(inventoryProductService.findOne('id123')).rejects.toThrow(
+        'product not found',
+      );
+    });
+
+    it('shold return error "Type of id invalid"', async () => {
+      const error = new Error();
+      error['path'] = '_id';
+
+      jest
+        .spyOn(productRepository, 'findOneById')
+        .mockImplementationOnce(() => {
+          throw error;
+        });
+
+      await expect(inventoryProductService.findOne('id123')).rejects.toThrow(
+        'Type of id invalid',
+      );
+    });
+
+    it('shold return generic error', async () => {
+      jest
+        .spyOn(productRepository, 'findOneById')
+        .mockRejectedValue(new Error());
+
+      await expect(inventoryProductService.findOne('id123')).rejects.toThrow(
         RpcException,
       );
     });

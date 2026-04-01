21e0ac338b71c3e6843053ed720bade0d0fde961
GUSTAV0-CRUZ <gustavocruzsantos39@gmail.com>
2026-03-31 22:00:54 -0300

    test(inventory-product): testa o metodo finAll do InventoryProductService

--- inventory-product/src/inventory-product/inventory-product.service.spec.ts
+++ inventory-product/src/inventory-product/inventory-product.service.spec.ts
@@ -1,9 +1,18 @@
+/* eslint-disable @typescript-eslint/unbound-method */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
 import { Test, TestingModule } from '@nestjs/testing';
 import { InventoryProductService } from './inventory-product.service';
 import { ProductRepository } from './repository/product.repository';
+import { StatusProductEnum } from './enums/status-product.enum';
+import { RpcException } from '@nestjs/microservices';
+
+const createProduct = () => ({
+  name: 'anyName',
+  status: StatusProductEnum.AVAILABLE,
+});
 
 describe('InventoryProductService', () => {
-  let service: InventoryProductService;
+  let inventoryProductService: InventoryProductService;
   let productRepository: ProductRepository;
 
   beforeEach(async () => {
@@ -12,16 +21,43 @@ describe('InventoryProductService', () => {
         InventoryProductService,
         {
           provide: ProductRepository,
-          useValue: {},
+          useValue: {
+            findAll: jest.fn(),
+          },
         },
       ],
     }).compile();
 
-    service = module.get<InventoryProductService>(InventoryProductService);
+    inventoryProductService = module.get<InventoryProductService>(
+      InventoryProductService,
+    );
     productRepository = module.get<ProductRepository>(ProductRepository);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(inventoryProductService).toBeDefined();
+  });
+
+  describe('findAll', () => {
+    it('shold return array of product', async () => {
+      const product = createProduct();
+
+      jest
+        .spyOn(productRepository, 'findAll')
+        .mockResolvedValue([product] as any);
+
+      const result = await inventoryProductService.findAll();
+
+      expect(productRepository.findAll).toHaveBeenCalledTimes(1);
+      expect(result).toEqual([product]);
+    });
+
+    it('shold return generic error', async () => {
+      jest.spyOn(productRepository, 'findAll').mockRejectedValue(new Error());
+
+      await expect(inventoryProductService.findAll()).rejects.toThrow(
+        RpcException,
+      );
+    });
   });
 });

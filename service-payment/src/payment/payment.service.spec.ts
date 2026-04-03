7311ac9103518b0e24284ee8c345b990fe652c99
GUSTAV0-CRUZ <gustavocruzsantos39@gmail.com>
2026-04-02 22:50:13 -0300

    test(service-payment): testa o metodo payment

--- service-payment/src/payment/payment.service.spec.ts
+++ service-payment/src/payment/payment.service.spec.ts
@@ -1,10 +1,13 @@
+/* eslint-disable @typescript-eslint/unbound-method */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
 import { Test, TestingModule } from '@nestjs/testing';
 import { PaymentService } from './payment.service';
 import { HistoryPaymentRepository } from './repository/history-payment.repository';
 import { StripeService } from '../stripe/stripe.service';
+import { RpcException } from '@nestjs/microservices';
 
 describe('PaymentService', () => {
-  let service: PaymentService;
+  let paymentService: PaymentService;
   let historyPaymentRepository: HistoryPaymentRepository;
   let stripeService: StripeService;
 
@@ -14,16 +17,20 @@ describe('PaymentService', () => {
         PaymentService,
         {
           provide: HistoryPaymentRepository,
-          useValue: {},
+          useValue: {
+            create: jest.fn(),
+          },
         },
         {
           provide: StripeService,
-          useValue: {},
+          useValue: {
+            createPaymentIntent: jest.fn(),
+          },
         },
       ],
     }).compile();
 
-    service = module.get<PaymentService>(PaymentService);
+    paymentService = module.get<PaymentService>(PaymentService);
     historyPaymentRepository = module.get<HistoryPaymentRepository>(
       HistoryPaymentRepository,
     );
@@ -31,6 +38,52 @@ describe('PaymentService', () => {
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(paymentService).toBeDefined();
+  });
+
+  describe('payment', () => {
+    it('should return the idPaymentIntent and clientSecret', async () => {
+      const amount = 123;
+      const idProduct = 'idProduct123';
+      const idPaymentIntent = 'idPaymentIntent123';
+
+      jest.spyOn(stripeService, 'createPaymentIntent').mockResolvedValue({
+        id: idPaymentIntent,
+        amount,
+        client_secret: idPaymentIntent,
+      } as any);
+
+      const result = await paymentService.payment({
+        idProduct,
+        amount,
+      });
+
+      expect(stripeService.createPaymentIntent).toHaveBeenCalledWith(
+        amount * 100,
+        'brl',
+      );
+      expect(historyPaymentRepository.create).toHaveBeenCalledWith({
+        idPaymentIntent,
+        idProduct,
+        amount,
+        currency: 'brl',
+      });
+      expect(result).toEqual({
+        idPaymentIntent,
+        clientSecret: idPaymentIntent,
+      });
+    });
+
+    it('should return generic error: RpcException', async () => {
+      jest
+        .spyOn(stripeService, 'createPaymentIntent')
+        .mockImplementationOnce(() => {
+          throw new Error();
+        });
+
+      await expect(paymentService.payment({} as any)).rejects.toThrow(
+        RpcException,
+      );
+    });
   });
 });

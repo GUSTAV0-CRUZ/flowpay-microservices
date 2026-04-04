eca71937769fee6927811e41043af6984536c912
GUSTAV0-CRUZ <gustavocruzsantos39@gmail.com>
2026-04-04 14:05:04 -0300

    feat(service-order): adiciona o client proxy para a comunicacao

---
+++ service-order/src/client-proxy/client-proxy.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { ClientProxyFactory, Transport } from '@nestjs/microservices';
+
+@Injectable()
+export class ClientProxyService {
+  constructor(private readonly configService: ConfigService) {}
+
+  getClientProxyInventoryProduct() {
+    return ClientProxyFactory.create({
+      transport: Transport.RMQ,
+      options: {
+        urls: [String(this.configService.get('REBBITMQ_URL'))],
+        queue: 'inventory-product',
+        queueOptions: {
+          durable: true,
+        },
+      },
+    });
+  }
+
+  getClientProxyServicePayment() {
+    return ClientProxyFactory.create({
+      transport: Transport.RMQ,
+      options: {
+        urls: [String(this.configService.get('REBBITMQ_URL'))],
+        queue: 'service-payment',
+        queueOptions: {
+          durable: true,
+        },
+      },
+    });
+  }
+}

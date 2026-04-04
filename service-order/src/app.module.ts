c63f683a0aa6fee73e1d65c6071c12be5ad07f5c
GUSTAV0-CRUZ <gustavocruzsantos39@gmail.com>
2026-04-03 23:19:36 -0300

    feat(docker-compose): adiciona db do service-orde

--- service-order/src/app.module.ts
+++ service-order/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { OrderModule } from './order/order.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), OrderModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(String(process.env.MONGODB_URL)),
+    OrderModule,
+  ],
   controllers: [],
   providers: [],
 })

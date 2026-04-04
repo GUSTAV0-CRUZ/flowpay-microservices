3b510eb629ce8ac5fe9fd0814e92304e0952b7b1
GUSTAV0-CRUZ <gustavocruzsantos39@gmail.com>
2026-04-04 12:30:06 -0300

    test(service-order): injeta o repository no service

--- service-order/src/order/order.service.spec.ts
+++ service-order/src/order/order.service.spec.ts
@@ -1,15 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { OrderService } from './order.service';
+import { OrderRepository } from './repository/order.respository';
 
 describe('OrderService', () => {
   let service: OrderService;
+  let orderRepository: OrderRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [OrderService],
+      providers: [
+        OrderService,
+        {
+          provide: OrderRepository,
+          useValue: {},
+        },
+      ],
     }).compile();
 
     service = module.get<OrderService>(OrderService);
+    orderRepository = module.get<OrderRepository>(OrderRepository);
   });
 
   it('should be defined', () => {

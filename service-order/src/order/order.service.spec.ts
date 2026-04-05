0c6efd2dea25d5d71c6e9f86d2ddf8c70c9ed923
GUSTAV0-CRUZ <gustavocruzsantos39@gmail.com>
2026-04-05 12:30:31 -0300

    test(service-order): testa o metodo buyProduct do orderService

--- service-order/src/order/order.service.spec.ts
+++ service-order/src/order/order.service.spec.ts
@@ -1,10 +1,15 @@
+/* eslint-disable @typescript-eslint/unbound-method */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
 import { Test, TestingModule } from '@nestjs/testing';
 import { OrderService } from './order.service';
 import { OrderRepository } from './repository/order.respository';
 import { ClientProxyService } from '../client-proxy/client-proxy.service';
+import { StatusProductEnum } from './enums/status-product.enum';
+import { of } from 'rxjs';
+import { RpcException } from '@nestjs/microservices';
 
 describe('OrderService', () => {
-  let service: OrderService;
+  let orderService: OrderService;
   let orderRepository: OrderRepository;
 
   const inventoryProductClientProxy = {
@@ -23,7 +28,10 @@ describe('OrderService', () => {
         OrderService,
         {
           provide: OrderRepository,
-          useValue: {},
+          useValue: {
+            findOneByIdProduct: jest.fn(),
+            changeStatus: jest.fn(),
+          },
         },
         {
           provide: ClientProxyService,
@@ -35,11 +43,87 @@ describe('OrderService', () => {
       ],
     }).compile();
 
-    service = module.get<OrderService>(OrderService);
+    orderService = module.get<OrderService>(OrderService);
     orderRepository = module.get<OrderRepository>(OrderRepository);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(orderService).toBeDefined();
+  });
+
+  describe('buyProduct', () => {
+    it('should return paymentIntent', async () => {
+      const idProduct = 'idProduct123';
+      const price = 1200;
+      const status = StatusProductEnum.AVAILABLE;
+
+      jest
+        .spyOn(orderRepository, 'findOneByIdProduct')
+        .mockResolvedValue({ idProduct, price, status } as any);
+      jest.spyOn(servicePaymentClientProxy, 'send').mockReturnValue(
+        of({
+          idPaymentIntent: 'idPaymentIntent',
+          clientSecret: 'clientSecret',
+        }),
+      );
+      jest
+        .spyOn(orderRepository, 'changeStatus')
+        .mockResolvedValue({ idProduct } as any);
+      jest.spyOn(inventoryProductClientProxy, 'emit');
+
+      const result = await orderService.buyProduct({ idProduct });
+
+      expect(orderRepository.findOneByIdProduct).toHaveBeenCalledWith(
+        idProduct,
+      );
+      expect(servicePaymentClientProxy.send).toHaveBeenCalledWith('payment', {
+        idProduct,
+        amount: price,
+      });
+      expect(orderRepository.changeStatus).toHaveBeenCalledWith(
+        idProduct,
+        StatusProductEnum.RESERVED,
+      );
+      expect(inventoryProductClientProxy.emit).toHaveBeenCalledWith(
+        'changeStatus-inventory',
+        {
+          id: idProduct,
+          changeStatusDto: StatusProductEnum.RESERVED,
+        },
+      );
+
+      expect(result).toEqual({
+        idPaymentIntent: 'idPaymentIntent',
+        clientSecret: 'clientSecret',
+      });
+    });
+
+    it('should return error: Product not found', async () => {
+      await expect(orderService.buyProduct({} as any)).rejects.toThrow(
+        'Product not found',
+      );
+    });
+
+    it('should return error: Product not AVAILABLE', async () => {
+      jest
+        .spyOn(orderRepository, 'findOneByIdProduct')
+        .mockResolvedValue({ status: StatusProductEnum.RESERVED } as any);
+
+      await expect(orderService.buyProduct({} as any)).rejects.toThrow(
+        'Product not AVAILABLE',
+      );
+    });
+
+    it('should return generic error: RpcException', async () => {
+      jest
+        .spyOn(orderRepository, 'findOneByIdProduct')
+        .mockRejectedValue(() => {
+          throw new Error();
+        });
+
+      await expect(orderService.buyProduct({} as any)).rejects.toThrow(
+        RpcException,
+      );
+    });
   });
 });

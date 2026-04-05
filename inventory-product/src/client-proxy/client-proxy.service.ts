d579207d881a3c3df2eae3c9407c9026cc6c623b
GUSTAV0-CRUZ <gustavocruzsantos39@gmail.com>
2026-04-05 18:00:34 -0300

    feat(inventory-product): adiciona o modulo do clientProxy para a comunicacao

---
+++ inventory-product/src/client-proxy/client-proxy.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { ClientProxyFactory, Transport } from '@nestjs/microservices';
+
+@Injectable()
+export class ClientProxyService {
+  constructor(private readonly configService: ConfigService) {}
+
+  getClientProxyServiceOrder() {
+    return ClientProxyFactory.create({
+      transport: Transport.RMQ,
+      options: {
+        urls: [String(this.configService.get('REBBITMQ_URL'))],
+        queue: 'service-order',
+        queueOptions: {
+          durable: true,
+        },
+      },
+    });
+  }
+}

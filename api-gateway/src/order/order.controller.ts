d71a2cbcd2388385f8b1c4336af17d356810affb
GUSTAV0-CRUZ <gustavocruzsantos39@gmail.com>
2026-04-06 10:54:06 -0300

    feat(api-gateway): adiciona o endpoint para solicitar o reembolso de um produto

--- api-gateway/src/order/order.controller.ts
+++ api-gateway/src/order/order.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { BuyProductDto } from './dtos/buy-product.dto';
 import { ClientProxyService } from '../client-proxy/client-proxy.service';
+import { ReversalProductDto } from './dtos/reversal-product.dto';
 
 @Controller('order')
 export class OrderController {
@@ -18,83 +19,11 @@ export class OrderController {
     return this.serviceOrderClientProxy.send('buyProduct-order', buyProductDto);
   }
 
-  // @EventPattern('reversalProduct-order')
-  // async reversalProduct(
-  //   @Payload() reversalProductDto: ReversalProductDto,
-  //   @Ctx() ctx: RmqContext,
-  // ) {
-  //   const channel = ctx.getChannelRef() as Channel;
-  //   const originalMsg = ctx.getMessage() as Message;
-
-  //   try {
-  //     await this.orderService.reversalProduct(reversalProductDto);
-  //     channel.ack(originalMsg);
-  //   } catch (error) {
-  //     catchWithMessageResilience(error, channel, originalMsg);
-  //   }
-  // }
-
-  // @EventPattern('addProduct-order')
-  // async addProduct(
-  //   @Payload() addProductDto: AddProductDto,
-  //   @Ctx() ctx: RmqContext,
-  // ) {
-  //   const channel = ctx.getChannelRef() as Channel;
-  //   const originalMsg = ctx.getMessage() as Message;
-
-  //   try {
-  //     await this.orderService.addProduct(addProductDto);
-  //     channel.ack(originalMsg);
-  //   } catch (error) {
-  //     catchWithMessageResilience(error, channel, originalMsg);
-  //   }
-  // }
-
-  // @EventPattern('removeProduct-order')
-  // async removeProduct(
-  //   @Payload() removeProductDto: RemoveProductDto,
-  //   @Ctx() ctx: RmqContext,
-  // ) {
-  //   const channel = ctx.getChannelRef() as Channel;
-  //   const originalMsg = ctx.getMessage() as Message;
-
-  //   try {
-  //     await this.orderService.removeProduct(removeProductDto);
-  //     channel.ack(originalMsg);
-  //   } catch (error) {
-  //     catchWithMessageResilience(error, channel, originalMsg);
-  //   }
-  // }
-
-  // @EventPattern('confirmOrder-order')
-  // async confirmOrder(
-  //   @Payload() confirmOrderDto: ConfirmOrderDto,
-  //   @Ctx() ctx: RmqContext,
-  // ) {
-  //   const channel = ctx.getChannelRef() as Channel;
-  //   const originalMsg = ctx.getMessage() as Message;
-
-  //   try {
-  //     await this.orderService.confirmOrder(confirmOrderDto);
-  //     channel.ack(originalMsg);
-  //   } catch (error) {
-  //     catchWithMessageResilience(error, channel, originalMsg);
-  //   }
-  // }
-
-  // @EventPattern('rollbackOrder-order')
-  // async rollbackOrder(
-  //   @Payload() rollbackOrderDto: RollbackOrderDto,
-  //   @Ctx() ctx: RmqContext,
-  // ) {
-  //   const channel = ctx.getChannelRef() as Channel;
-  //   const originalMsg = ctx.getMessage() as Message;
-
-  //   try {
-  //     await this.orderService.rollback(rollbackOrderDto);
-  //     channel.ack(originalMsg);
-  //   } catch (error) {
-  //     catchWithMessageResilience(error, channel, originalMsg);
-  //   }
-  // }
+  @Post('reversalProduct')
+  reversalProduct(@Body() reversalProductDto: ReversalProductDto) {
+    return this.serviceOrderClientProxy.emit(
+      'reversalProduct-order',
+      reversalProductDto,
+    );
+  }
 }

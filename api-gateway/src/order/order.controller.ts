4dd3a03c0eb9bcfc1cc53159e6989c9bc89cff26
GUSTAV0-CRUZ <gustavocruzsantos39@gmail.com>
2026-04-07 19:27:09 -0300

    feat(api-gateway): adiciona o response do order no swagger

--- api-gateway/src/order/order.controller.ts
+++ api-gateway/src/order/order.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { BuyProductDto } from './dtos/buy-product.dto';
 import { ClientProxyService } from '../client-proxy/client-proxy.service';
 import { ReversalProductDto } from './dtos/reversal-product.dto';
+import { ApiResponse } from '@nestjs/swagger';
 
 @Controller('order')
 export class OrderController {
@@ -13,12 +14,22 @@ export class OrderController {
       clientProxyService.getClientProxyServiceOrder();
   }
 
+  @ApiResponse({
+    status: 201,
+    schema: {
+      type: 'object',
+      properties: {
+        idPaymentIntent: { type: 'string' },
+        clientSecret: { type: 'string' },
+      },
+    },
+  })
   @Post('buyProduct')
   buyProduct(@Body() buyProductDto: BuyProductDto) {
-    console.log(buyProductDto);
     return this.serviceOrderClientProxy.send('buyProduct-order', buyProductDto);
   }
 
+  @HttpCode(HttpStatus.ACCEPTED)
   @Post('reversalProduct')
   reversalProduct(@Body() reversalProductDto: ReversalProductDto) {
     return this.serviceOrderClientProxy.emit(

03b90114233f0df48cb18ac90a69a58476451823
GUSTAV0-CRUZ <gustavocruzsantos39@gmail.com>
2026-04-07 19:06:52 -0300

    feat(api-gateway): adiciona e configura o swagger no gateway

--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -3,9 +3,21 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { AllExceptionFilter } from './common/filters/all-exception.filter';
 import { raw } from 'express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('FlowPay')
+    .setDescription('Microservice de uma api de pagamentos')
+    .setVersion('1.0')
+    .addTag('Api-gateway')
+    .build();
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+
+  SwaggerModule.setup('api', app, swaggerDocument);
+
   app.use('/webhook/payment-stripe', raw({ type: 'application/json' }));
   app.useGlobalPipes(
     new ValidationPipe({
